f68ff848fd2625ad0c582fb1c95d13feef21d5a2
qbaHub <jakub.moskwa@gmail.com>
2020-06-23 14:29:57 +0200

    Merge branch 'dungeon_jm' of https://github.com/cleobfz/Character

--- CharacterSheet/src/model/Dungeon.java
+++ CharacterSheet/src/model/Dungeon.java
@@ -3,14 +3,20 @@ package model;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.text.IconView;
+
 
 public class Dungeon extends JPanel{
 	static private int[][] dungeon3x3 = {{0, 1, 2},	{3, 4, 5},	{6, 7, 8}};
@@ -18,7 +24,8 @@ public class Dungeon extends JPanel{
 	private int y;
 	private int p;//position
 	static private int size = 198;
-	private String c;//character 
+	static private Character c;//character
+	private Icon ic = c.getOrigin().getIcon();
 	
 	public Dungeon(int position) {
 		this.x = 0;
@@ -33,11 +40,12 @@ public class Dungeon extends JPanel{
 		});
 	}
 	
-	public Dungeon(String character, int position) {
-		c = character;
+	public Dungeon(Character character, int position) {
+		this.c = character;
 		this.p = position;
 		this.x = -1;
 		this.y = -1;
+		this.ic = this.c.getOrigin().getIcon();
 		for (int xIndex = 0; xIndex < dungeon3x3.length; xIndex++) {
 			for (int yIndex = 0; yIndex < dungeon3x3.length; yIndex++) {
 				if (dungeon3x3[xIndex][yIndex] == this.p) {
@@ -60,23 +68,14 @@ public class Dungeon extends JPanel{
 		requestFocus();
 	}
 	
-//	public void paintComponent(Graphics g) {
-//		int x = this.x * (size/3);
-//		int y = this.y * (size/3);
-//		g.setColor(new Color(32, 32, 32));
-//		g.clearRect(0, 0, getWidth(), getHeight());
-//		g.fillRect(x, y, (size/3), (size/3));
-//	}
-				
-		
-	public void paintComponent(Graphics g) {
+	public void paint(Graphics g) {
 		int x = this.x * (size/3);
-		int y = this.y * (size/3) + 10;
-		g.setColor(new Color(32, 32, 32));
+		int y = this.y * (size/3);
 		g.clearRect(0, 0, getWidth(), getHeight());
-		g.drawString(c, x, y);
+		ImageIcon icon = (ImageIcon) ic;
+		ImageIcon icon2 = new ImageIcon(icon.getImage().getScaledInstance(size/3, size/3, Image.SCALE_SMOOTH));
+		icon2.paintIcon(this, g, x, y);
 	}
-	
 
 	public int getX() {
 		return x;
@@ -114,18 +113,6 @@ public class Dungeon extends JPanel{
 		this.p = dungeon3x3[this.x][this.y];
 	}
 	
-//	public void move(int keyCode) {
-//		int kc = keyCode;
-//		switch (kc) {
-//		case (39): if ( this.getX() < 2) {this.x = this.getX() + 1; repaint();}; break;
-//		case (37): if ( this.getX() > 0) {this.x = this.getX() - 1; repaint();}; break;
-//		case (40): if ( this.getY() < 2) {this.y = this.getY() + 1; repaint();}; break;
-//		case (38): if ( this.getY() > 0) {this.y = this.getY() - 1; repaint();}; break;
-//		default: break; //jede andere Taste - mach nichts
-//		}
-//		setPosition();
-//	}
-	
 	public int[] move(int keyCode) {
 		int[] xyk = new int[2]; 
 		int kc = keyCode;
@@ -139,42 +126,51 @@ public class Dungeon extends JPanel{
 		setPosition();
 		xyk[0] = this.getX();
 		xyk[1] = this.getY();
+		System.out.println("neu x: " + xyk[0] + ", new y: " + xyk[1] + ", position: " + this.getPosition());
 		return xyk;		
 	}
 
 
 	
 	//TEST:
-	public static void main(String[] args) {
-		JFrame mf;
-		mf = new JFrame();
-		mf.setTitle("Dungeon");
-		mf.setLocation(100,100);
-		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		BoxLayout mainLayout = new BoxLayout(mf.getContentPane(), BoxLayout.Y_AXIS);
-		mf.setLayout(mainLayout);
-
-//		Dungeon d = new Dungeon(4);
-//		mf.add(d);
-		
-		Dungeon d2 = new Dungeon("Dungeon", 4);
-		int[] newPosition = d2.move(39); 
-		System.out.println("new x: " + newPosition[0] + ", new y: " + newPosition[1]);
-		mf.add(d2);
-//		d2.setPosition(4);
-//		System.out.println(d2.getPosition());
-//		System.out.println(d2.getX());
-//		System.out.println(d2.getY());
-//		System.out.println(d2.getPosition());
-//		d2.setPosition(3);
-//		System.out.println(d2.getX());
-//		System.out.println(d2.getY());
-//		System.out.println(d2.getPosition());
-		
-		
-		mf.pack();
-		mf.setVisible(true);
-		
-	}
+//	public static void main(String[] args) {
+//		JFrame mf;
+//		mf = new JFrame();
+//		mf.setTitle("Dungeon");
+//		mf.setLocation(100,100);
+//		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		
+//		BoxLayout mainLayout = new BoxLayout(mf.getContentPane(), BoxLayout.Y_AXIS);
+//		mf.setLayout(mainLayout);
+//
+////		Dungeon d = new Dungeon(4);
+////		mf.add(d);
+//		
+//		//Character füt Test
+//		ArrayList<Item> inv = new ArrayList<>();
+//		inv.add(new Item("Peng", 2,2));
+//		inv.add(new Item("Pow", 4,5));
+//		ArrayList<Skill> skills = new ArrayList<>();
+//		skills.add(new Skill("Pew pew", 5));
+//		
+//		
+//		c = new Character("Odlon", 3, 3, inv, skills, new Origin("Dwarf","Krieger"));
+//		Dungeon d2 = new Dungeon(c, 2);
+//		//int[] newPosition = d2.move(39);
+//		mf.add(d2);
+////		d2.setPosition(4);
+////		System.out.println(d2.getPosition());
+////		System.out.println(d2.getX());
+////		System.out.println(d2.getY());
+////		System.out.println(d2.getPosition());
+////		d2.setPosition(3);
+////		System.out.println(d2.getX());
+////		System.out.println(d2.getY());
+////		System.out.println(d2.getPosition());
+//		
+//		
+//		mf.pack();
+//		mf.setVisible(true);
+//		
+//	}//end of main
 }
