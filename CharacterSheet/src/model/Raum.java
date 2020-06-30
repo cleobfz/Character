933e21293d1fdfdf410891951465fa2523be3cd4
julee09 <julee09@t-online.de>
2020-06-30 08:15:03 +0200

    Room

--- CharacterSheet/src/model/Raum.java
+++ CharacterSheet/src/model/Raum.java
@@ -5,12 +5,9 @@ import java.io.Serializable;
 public class Raum implements Serializable {
 
 	private static final long serialVersionUID = -8754566594733954539L;
-Monster Gegner;
- Item Schatz;
- boolean Ausgang;
- 
- 
- 
+	Monster Gegner;
+	Item Schatz;
+	boolean Ausgang;
 
 public Raum(Monster gegner, Item schatz, boolean Ausgang) {
 	this.Ausgang = Ausgang;
@@ -21,24 +18,28 @@ public Raum(Monster gegner, Item schatz, boolean Ausgang) {
 public Monster getGegner() {
 	return Gegner;
 	}
+
 public void setGegner(Monster gegner) {
 	Gegner = gegner;
-}
+	}
+
 public Item getSchatz() {
 	return Schatz;
-}
+	}
+
 public void setSchatz(Item schatz) {
 	Schatz = schatz;
 	}
+
 public boolean getAusgang(){
 	return Ausgang;
-}
+	}
+
 
 @Override
 public String toString() {
-	return "\n Gegner=" + Gegner + "\n Schatz: " + Schatz + "\n Ausgang: " + Ausgang + "";
+	return "\n Gegner:" + Gegner + "\n Schatz: " + Schatz + "\n Ausgang: " + Ausgang + "";
 }
-
 
 
 }
